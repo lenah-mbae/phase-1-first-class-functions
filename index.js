06a32bcc2c624f3f9ce76cd010aba1bfda5d690e
lenahmbae <lenahmbae22@gmail.com>
2025-03-27 18:15:41 +0300

    first class functions

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+  function returnsANamedFunction() {
+    return function namedFunction() { 
+      console.log("I am a named function!");
+    };
+  }
+  
+ 
+  function returnsAnAnonymousFunction() {
+    return function() { 
+      console.log("I am anonymous!");
+    };
+  }
+  
+
+  module.exports = {
+    receivesAFunction,
+    returnsANamedFunction,
+    returnsAnAnonymousFunction
+  };
